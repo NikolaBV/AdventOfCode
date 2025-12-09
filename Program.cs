9da8b7f75a73019390003033d4568048337d2164
Nikola Valkov <bulgarianmapper64@gmail.com>
2025-12-09 16:57:08 +0200

    Fixed up code structure a little

--- Program.cs
+++ Program.cs
@@ -1,67 +1,89 @@
 ﻿using System;
 
-/*
-1. Read file
-2. Extract values into pairs of direction (key) and distance (value) 
-3. 
-*/
-
-String line;
-int value = 50;
-int zeroCounter = 0;
-try
+void DayOne()
 {
-    StreamReader streamReader = new StreamReader("C:\\Programming\\C#\\AdventOfCode\\assets\\day1\\rotations.txt");
-    line = streamReader.ReadLine();
-
-    while (line != null)
+    String line;
+    int value = 50;
+    int zeroCounter = 0;
+    try
     {
-        Console.WriteLine(line);
-
-        char direction = line[0];
-        string distanceInString = "";
-
-        if(line.Length > 3)
-        {
-            Console.WriteLine("");
-        }
-        distanceInString = line.Substring(1, line.Length -1);
-
-        int distance = Int32.Parse(distanceInString);
-
-        if (direction == 'L')
-        {
-            value = value - distance;
-
-        }
-        else if (direction == 'R')
-        {
-            value = value + distance;
-        }
-
-        while(value < 0)
-        {
-            value = value + 100;
-        }
-
-        while(value > 99)
-        {
-            value = value - 100;
-        }
-
-        if (value == 0)
-        {
-            zeroCounter = zeroCounter + 1;
-        }
+        StreamReader streamReader = new StreamReader("C:\\Programming\\C#\\AdventOfCode\\assets\\day1\\rotations.txt");
         line = streamReader.ReadLine();
+
+        while (line != null)
+        {
+            Console.WriteLine(line);
+
+            char direction = line[0];
+            string distanceInString = "";
+
+            if (line.Length > 3)
+            {
+                Console.WriteLine("");
+            }
+            distanceInString = line.Substring(1, line.Length - 1);
+
+            int distance = Int32.Parse(distanceInString);
+
+            if (direction == 'L')
+            {
+                value = value - distance;
+
+            }
+            else if (direction == 'R')
+            {
+                value = value + distance;
+            }
+
+            while (value < 0)
+            {
+                value = value + 100;
+            }
+
+            while (value > 99)
+            {
+                value = value - 100;
+            }
+
+            if (value == 0)
+            {
+                zeroCounter = zeroCounter + 1;
+            }
+            line = streamReader.ReadLine();
+        }
+
+        streamReader.Close();
+        Console.ReadLine();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception: " + e.Message);
     }
 
-    streamReader.Close();
-    Console.ReadLine();
-}
-catch (Exception e)
-{
-    Console.WriteLine("Exception: " + e.Message);
+    Console.WriteLine("Password is: " + zeroCounter);
 }
 
-Console.WriteLine("Password is: " + zeroCounter);
+
+void DayTwo()
+{
+    String line;
+
+    try
+    {
+        StreamReader streamReader = new StreamReader("C:\\Programming\\C#\\AdventOfCode\\assets\\day1\\rotations.txt");
+        line = streamReader.ReadLine();
+
+        while (line != null)
+        {
+            Console.WriteLine(line);
+        }
+
+        streamReader.Close();
+        Console.ReadLine();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception: " + e.Message);
+    }
+}
+//DayOne();
